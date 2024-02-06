2c153c2f5a8c98213f274fe5581512f62da4da8e
mahdiieh <shmahdieh773@gmail.com>
2024-02-06 21:39:55 +0330

    add attributes and methods of order class.

--- main/src/Order.java
+++ main/src/Order.java
@@ -1,4 +1,16 @@
 public class Order {
+    private int d_orderId;
+    private int d_playerId;
 
+    private enum d_orderType{Deploy};
+    private int d_timeStamp;
+
+    public void ExecuteOrder(Player p_player, d_orderType p_orderType){
+
+    }
+
+    public void ValidateOrder(Player p_player){
+
+    }
 
 }
