4d03d67a829be9f1119b0e2bf5ab3ba785e58094
ammiroo <amirmohammadrezaeipour98@gmail.com>
2024-02-06 13:59:57 -0500

    add continent class and methods.

--- main/src/Continent.java
+++ main/src/Continent.java
@@ -1,2 +1,17 @@
 public class Continent {
+
+    int continentId;
+    String continentName;
+    int continentValue;
+
+    public Continent(int continentId, String continentName, int continentValue) {
+        this.continentId = continentId;
+        this.continentName = continentName;
+        this.continentValue = continentValue;
+    }
+
+    int getBonusTroops() {
+
+        return continentValue;
+    };
 }
