f4d3bb428526753d5127ad59b624cd49f5a0921d
mahdiieh <shmahdieh773@gmail.com>
2024-02-06 21:11:45 +0330

    add attributes and methods of player class.

--- main/src/Player.java
+++ main/src/Player.java
@@ -1,2 +1,23 @@
 public class Player {
+    private int playerId;
+    private String name;
+    private int playerStat;
+    private int reinforcement;
+    private Order orders;
+    private Country countries;
+
+    public void IssueOrder(){
+        //“issue_order()” (no parameters, no return value) whose function is to add an order to the list of orders held by the
+        //player when the game engine calls it during the issue orders phase.
+
+    }
+
+    public Order NextOrder(){
+        //“next_order()” (no parameters) method that is called by the GameEngine during the execute orders phase and
+        //returns the first order in the player’s list of orders, then removes it from the list.
+        Order o = new Order();
+        return o;
+    }
+
+
 }
