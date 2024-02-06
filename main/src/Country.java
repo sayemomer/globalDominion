a58968e657ad87e1fbad8a5bd0c07d13b010b317
FaridFarahmand <faridfarahmandf2f@gmail.com>
2024-02-06 12:52:22 -0500

    initialize country class

--- main/src/Country.java
+++ main/src/Country.java
@@ -1,5 +1,43 @@
 public class Country {
+    private int countryId;
+    private String countryName;
+    private String[] adjacentCountries;
+
+    public Country(int countryId, String countryName, String[] adjacentCountries) {
+        this.countryId = countryId;
+        this.countryName = countryName;
+        this.adjacentCountries = adjacentCountries;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String[] getAdjacentCountries() {
+        return adjacentCountries;
+    }
+
+    public void setAdjacentCountries(String[] adjacentCountries) {
+        this.adjacentCountries = adjacentCountries;
+    }
+
+    public void setPlayer() {}
+    public void clearPlayer() {}
+    public void editNeighbor() {}
     public static void main(String[] args) {
-        System.out.println("Hello map");
+
+
     }
 }
