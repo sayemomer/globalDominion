9258b0be50f94004cce560a7e065b4e0c4659001
parsamre <parsa.mre@gmail.com>
2024-02-08 15:18:47 -0500

    add basic functioning startup phase. this commit only contains support for player add/remove commands.

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -1,4 +1,66 @@
+import controllers.Command;
+import controllers.PlayerController;
+import models.Player;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class GameEngine {
+
+    ArrayList<Player> players;
+    Scanner scanner;
+    PlayerController playerController;
+
+    public GameEngine() {
+        scanner = new Scanner(System.in);
+        playerController = new PlayerController();
+        this.players = new ArrayList<>();
+        int phaseResult = 0;
+        phaseResult = startUpPhase();
+        if (phaseResult == 1) {
+            return;
+        }
+
+    }
+
+    int startUpPhase() {
+        System.out.println("*-*-* STARTUP PHASE *-*-*");
+        System.out.println("In this phase you can: ");
+        System.out.println(" - Add/Remove players to/from the game.");
+        System.out.println(" - Assign countries to players.");
+        System.out.println(" - Load and edit maps.");
+        System.out.println("Type 'help' to see the list of available commands.");
+        System.out.println("Type 'proceed' to proceed to the next phase.");
+        System.out.println("Type 'back' to go back to the main menu.");
+
+        while (true) {
+
+            String[] inputString = scanner.nextLine().toLowerCase().split("\\s+");
+
+            if (inputString.equals("proceed")) {
+                System.out.println("Proceeding to the next phase...");
+                break;
+            } else if (inputString.equals("exit")) {
+                System.out.println("Exiting the game...");
+                return 1;
+            } else if (inputString.equals("help")) {
+                System.out.println("Available commands: ");
+                System.out.println("  " + Command.GAME_PLAYER_SYNTAX);
+                System.out.println("  " + Command.LOAD_MAP_SYNTAX);
+                System.out.println("  " + Command.ASSIGN_COUNTRIES_SYNTAX);
+            } else if (inputString[0].equals(Command.GAME_PLAYER)) {
+                playerController.handleGamePlayerCommand(Arrays.copyOfRange(inputString, 1, inputString.length));
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+        return 0;
+
+    }
+
+
     void startGameLoop() {
         System.out.println("Game loop is running...");
         assignReinforcementsPhase();
