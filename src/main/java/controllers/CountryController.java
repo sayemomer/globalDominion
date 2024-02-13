8cf39de2557f6039178dfcb7e12bee67a8cc7c86
parsamre <parsa.mre@gmail.com>
2024-02-13 15:08:19 -0500

    add functionalities to add remove countries and continents.

--- src/main/java/controllers/CountryController.java
+++ src/main/java/controllers/CountryController.java
@@ -5,6 +5,7 @@ import java.util.*;
 import models.Country;
 import models.GameState;
 import models.Player;
+import models.Continent;
 
 public class CountryController {
     GameState d_gameState;
@@ -94,4 +95,112 @@ public class CountryController {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     * This method is used to handle the edit country command.
+     *
+     * @param p_args command arguments
+     */
+    public void handleEditCountryCommand(String[] p_args) {
+        try {
+            if (p_args.length != 3)
+                throw new Exception("Invalid number of arguments." + "Correct Syntax: \n\t" + Command.EDIT_COUNTRY_SYNTAX);
+            String l_option = p_args[0].toLowerCase();
+            int l_countryId = Integer.parseInt(p_args[1]);
+            int l_continentID = Integer.parseInt(p_args[2]);
+            if (l_option.equals(Command.ADD)) {
+                if (d_gameState.getCountries().containsKey(l_countryId))
+                    throw new Exception("Country already exists.");
+                if (!d_gameState.getContinents().containsKey(l_continentID))
+                    throw new Exception("Continent does not exist.");
+
+                d_gameState.getCountries().put(l_countryId, new Country(l_countryId, "", l_continentID));
+            } else if (l_option.equals(Command.REMOVE)) {
+                if (!d_gameState.getCountries().containsKey(l_countryId))
+                    throw new Exception("Country does not exist.");
+                d_gameState.getCountries().remove(l_countryId);
+                removeRelatedConnectionsToCountry(l_countryId);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public void handleEditContinentCommand(String[] p_args) {
+        try {
+            if (!(p_args.length == 3 || p_args.length == 2))
+                throw new Exception("Invalid number of arguments." + "Correct Syntax: \n\t" + Command.EDIT_CONTINENT_SYNTAX);
+            String l_option = p_args[0].toLowerCase();
+            int l_continentId = Integer.parseInt(p_args[1]);
+
+            if (l_option.equals(Command.ADD)) {
+                if (p_args.length != 3)
+                    throw new Exception("Invalid number of arguments." + "Correct Syntax: \n\t" + Command.EDIT_CONTINENT_SYNTAX);
+                int l_bonus = Integer.parseInt(p_args[2]);
+                if (d_gameState.getContinents().containsKey(l_continentId))
+                    throw new Exception("Continent already exists.");
+                d_gameState.getContinents().put(l_continentId, new Continent(l_continentId, l_bonus));
+            } else if (l_option.equals(Command.REMOVE)) {
+                if (!d_gameState.getContinents().containsKey(l_continentId))
+                    throw new Exception("Continent does not exist.");
+                d_gameState.getContinents().remove(l_continentId);
+                removeRelatedCountriesToContinent(l_continentId);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * This method is used to remove related countries to a continent.
+     * uses removeRelatedConnectionsToCountry to remove connections to the country
+     *
+     * @param continentID continent ID
+     */
+    private void removeRelatedCountriesToContinent(int continentID) {
+        StringBuilder outputString = new StringBuilder("As an effect to previous action, the following countries are removed:\n");
+        ArrayList<Country> deletedCountries = new ArrayList<>();
+        d_gameState.getCountries().forEach((id, country) -> {
+            if (country.getContinentId() == continentID) {
+                deletedCountries.add( d_gameState.getCountries().remove(id));
+            }
+        });
+        outputString.append("Removed countries: ");
+        deletedCountries.forEach(country -> outputString.append(country.getCountryId()).append(", "));
+        outputString.append("\n");
+
+        if (deletedCountries.isEmpty()) {
+            return;
+        }
+
+        System.out.println(outputString);
+
+        for (Country deletedCountry : deletedCountries) {
+            removeRelatedConnectionsToCountry(deletedCountry.getCountryId());
+        }
+    }
+
+    /**
+     * This method is used to remove related connections to a country.
+     * used in removeRelatedCountriesToContinent
+     *
+     * @param countryID country ID
+     */
+    private void removeRelatedConnectionsToCountry(int countryID) {
+        StringBuilder outputString = new StringBuilder("As an effect to previous action, the following connections are removed:\n");
+        Country deletedCountry = d_gameState.getCountries().get(countryID);
+        d_gameState.getCountries().forEach((id, country) -> {
+            if (country.getAdjacentCountries().contains(countryID)) {
+                outputString.append(" ").append(country.getCountryId()).append(" -> ").append(deletedCountry.getCountryId()).append("\n");
+                country.getAdjacentCountries().remove(countryID);
+            }
+        });
+
+        System.out.println(outputString);
+    }
+
+
+
 }
