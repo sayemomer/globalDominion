d8462f193821ed370d3119687ef696ea71d71255
mahdiieh <shmahdieh773@gmail.com>
2024-02-15 04:30:08 +0330

    add tests for CountryControllerTest related to removing countries.

--- src/test/java/controllers/CountryControllerTest.java
+++ src/test/java/controllers/CountryControllerTest.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Continent;
 import models.Country;
 import models.GameState;
 import models.Player;
@@ -15,7 +16,9 @@ class CountryControllerTest {
     private GameState gameState;
     private CountryController countryController;
     private ArrayList<Player> players;
+
     private Map<Integer, Country> countries;
+    private Map<Integer, Continent> continents;
 
     @BeforeEach
     void setUp() {
@@ -23,11 +26,7 @@ class CountryControllerTest {
         countryController = new CountryController(gameState);
         players = gameState.getPlayers();
         countries = gameState.getCountries();
-    }
 
-
-    @Test
-    void handleAssignCountries() {
         players.add(new Player(1,"farid"));
         players.add(new Player(2, "parsa"));
         players.add(new Player(3,"Mahdie"));
@@ -36,6 +35,16 @@ class CountryControllerTest {
         countries.put(2,new Country(2, "Canada",3));
         countries.put(3,new Country(3, "England",2));
 
+        countries.get(1).addAdjacentCountry(2);
+        countries.get(2).addAdjacentCountry(3);
+        countries.get(3).addAdjacentCountry(1);
+
+    }
+
+
+    @Test
+    void handleAssignCountries() {
+
         countryController.handleAssignCountriesCommand(new String[] {});
 
         for (Player player:
@@ -43,4 +52,24 @@ class CountryControllerTest {
             assertEquals(1, player.getCountries().size());
         }
     }
+
+    @Test
+    void shouldNotAddCountryToNonExistingContinent() {
+        countryController.handleEditCountryCommand(new String[] {"-add", "7", "7"});
+        assertFalse(gameState.getCountries().containsKey(7));
+    }
+
+    @Test
+    void shouldRemoveRelatedCountriesWhenContinentRemoved() {
+        countryController.handleEditContinentCommand(new String[] {"-remove", "1"});
+        assertFalse(gameState.getCountries().containsKey(1));
+    }
+
+    @Test
+    void shouldRemoveRelatedConnectionsWhenCountryRemoved() {
+        String[] args = {"-remove", "1"};
+        countryController.handleEditCountryCommand(args);
+        assertFalse(gameState.getCountries().get(3).getAdjacentCountries().contains(1));
+    }
+
 }
