3adb3c3e8cd6bdb9907bd175953d26f25e74c23b
assassin008 <djdkdc1234@gmail.com>
2024-03-11 13:48:11 -0400

    add log_entry_buffer,log_observer class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import config.AppConfig;
 import config.Debug;
+import models.FileLogObserver;
+import models.LogEntryBuffer;
 
 public class App {
     public static void main(String[] args) {
@@ -10,6 +12,13 @@ public class App {
         Debug.log("Game started.");
         Debug.log("verboseMode is " + (AppConfig.isVerboseMode() ? "on" : "off"));
         Debug.log("debugMode is " + (AppConfig.isDebugMode() ? "on" : "off"));
+
+
+        LogEntryBuffer logBuffer = new LogEntryBuffer();
+        FileLogObserver observer = new FileLogObserver("log_file.txt");
+        logBuffer.addObserver(observer);
+        logBuffer.logAction("Game started.");
+
 
         GameMenu gameMenu = new GameMenu();
         gameMenu.start();
