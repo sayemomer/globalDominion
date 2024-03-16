b783c60eba6f0179dbedc256c0735a6b4ec1fda3
sayemomer <omer_sayem@yahoo.com>
2024-03-16 14:50:12 -0400

    new testsuite , updated readme and test coverage tool

---
+++ src/test/java/models/orders/OrderTestSuite.java
@@ -0,0 +1,10 @@
+package models.orders;
+
+import org.junit.platform.suite.api.Suite;
+
+import org.junit.platform.suite.api.SelectPackages;
+
+@SelectPackages("models.orders")
+@Suite
+public class OrderTestSuite {
+}
