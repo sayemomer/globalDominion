60dd97cac304e5e83832f98de3f067055160dd11
mahdiieh <shmahdieh773@gmail.com>
2024-03-28 22:29:08 +0330

    fixed playerBehavior class

--- src/main/java/models/behaviors/PlayerBehavior.java
+++ src/main/java/models/behaviors/PlayerBehavior.java
@@ -1,5 +1,22 @@
 package models.behaviors;
 
+import models.Country;
+import models.Player;
+
+import java.util.List;
+
 public abstract class PlayerBehavior {
+    Player d_player;
+    List<Country> d_countries;
+
+    public PlayerBehavior(Player p_player, List<Country> p_countries){
+        d_player = p_player;
+        d_countries = p_countries;
+    }
     public abstract void issueOrders();
+    public abstract Country toAttack();
+    public abstract Country toAttackFrom();
+    public abstract Country toMoveFrom();
+    public abstract Country toDefend();
+
 }
