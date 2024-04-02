eb64fa46429e99a935c82dd816060c83efef849c
FaridFarahmand <faridfarahmandf2f@gmail.com>
2024-04-02 12:59:50 -0400

    randomStrategy with test

--- src/main/java/models/behaviors/RandomStrategyBehavior.java
+++ src/main/java/models/behaviors/RandomStrategyBehavior.java
@@ -1,58 +1,59 @@
-//package models.behaviors;
-//
-//import models.Country;
-//import models.GameState;
-//import models.Player;
-//import models.orders.AdvanceOrder;
-//import models.orders.DeployOrder;
-//import models.orders.Order;
-//
-//import java.util.List;
-//import java.util.Map;
-//import java.util.Random;
-//
-//public class RandomStrategyBehavior extends StrategyBehavior {
-//    // ConcreteStrategy of the Strategy pattern
-//    Random rand = new Random();
-//    GameState d_gameState;
-//
-//    public RandomStrategyBehavior(Player p_player, List<Country> p_countries) {
-//        super(p_player, (Map<Integer, Country>) p_countries);
-//    }
-//
-//    // Random country to move to
-//    public Country getAttackTo() {
-//        return(d_countries.get(rand.nextInt(d_countries.size() - 1)));
-//    }
-//
-//    // Random of its own countries to defend
-//    public Country getDeployIn() {
-//        return d_player.getCountries().get(rand.nextInt(d_player.getCountries().size() - 1));
-//    }
-//
-//    // Random of its own countries to attack from
-//    public Country getAttackFrom() {
-//        return getDeployIn();
-//    }
-//
-//    // Random of its own countries to move from
-//    public Country toMoveFrom() {
-//        return getDeployIn();
-//    }
-//
-//    // The Random player can either deploy or advance, determined randomly. .
-//    public Order issueOtherOrders() {
-//        int randomOrder = rand.nextInt(3);
-//        int random_number_of_armies;
-//        if (rand.nextInt(5) != 0) {
-//            switch (randomOrder) {
-//                case (0):
-//                    return new DeployOrder(d_gameState,d_player, getDeployIn().getCountryId(), rand.nextInt(20));
-//                case (1):
-//                    return new AdvanceOrder(d_gameState,d_player, toMoveFrom().getCountryId(), getAttackTo().getCountryId(), rand.nextInt(toMoveFrom().getArmies() + 5));
-//            }
-//        }
-//        return null;
-//    }
-//
-//}
+package models.behaviors;
+
+import models.Country;
+import models.GameState;
+import models.Player;
+import models.orders.AdvanceOrder;
+import models.orders.DeployOrder;
+import models.orders.Order;
+
+import java.util.*;
+
+/**
+ * RandomStrategyBehavior: computer player strategy that deploys on a random country, attacks random neighboring countries, and moves armies
+ * randomly between its countries.
+ */
+public class RandomStrategyBehavior extends StrategyBehavior {
+    // ConcreteStrategy of the Strategy pattern
+    Random rand = new Random();
+
+    /**
+     *
+     * @param p_player player
+     * @param p_gameState gameState
+     *
+     */
+    public RandomStrategyBehavior(Player p_player, GameState p_gameState) {
+        super(p_player, p_gameState);
+    }
+
+    // The Random player can either deploy or advance, determined randomly. .
+    /**
+     * Randomly selects a country owned by the player and issues an advance order from that country
+     * to one of its adjacent countries with a random number of armies.
+     *
+     * @return An AdvanceOrder object representing the order issued.
+     */
+
+    public Order issueOtherOrders() {
+                    Map<Integer, Country> playerCountries = d_player.getCountries();
+                    int randomPlayerCountryId = d_player.getCountryIds().get(rand.nextInt(d_player.getCountryIds().size()));
+                    int randomAdjacentCountry = playerCountries.get(randomPlayerCountryId).getAdjacentCountries().get(rand.nextInt(playerCountries.get(randomPlayerCountryId).getAdjacentCountries().size()));
+                    return new AdvanceOrder(d_gameState,d_player, randomPlayerCountryId , randomAdjacentCountry, d_player.getReinforcementPoll());
+    }
+
+    /**
+     * Issues a deploy order for the player by randomly selecting one of the player's countries
+     * and deploying a random number of armies to that country.
+     *
+     * @return A DeployOrder object representing the order issued.
+     */
+
+    @Override
+    protected Order issueDeployOrder() {
+        ArrayList<Integer> playerCountries =  d_player.getCountryIds();
+
+        return new DeployOrder(d_gameState, d_player, playerCountries.get(rand.nextInt(playerCountries.size())), d_player.getReinforcementPoll());
+    }
+
+}
---
+++ src/test/java/models/behaviors/RandomStrategyBehaviorTest.java
@@ -0,0 +1,141 @@
+package models.behaviors;
+
+import controllers.OrderController;
+import controllers.PlayerController;
+import models.Continent;
+import models.Country;
+import models.GameState;
+import models.Player;
+import models.orders.AdvanceOrder;
+import models.orders.DeployOrder;
+import models.orders.Order;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import phases.GameEngine;
+import phases.IssueDeployOrder;
+import phases.IssueOrdersPhase;
+
+import java.util.Map;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomStrategyBehaviorTest {
+    private GameState gameState;
+    private GameEngine gameEngine;
+    private static OrderController orderController;
+    private PlayerController playerController;
+    private Map<String, Player> players;
+    private Map<Integer, Country> countries;
+    private Map<Integer, Continent> continents;
+    Random rand = new Random();
+
+    @BeforeEach
+    void setUp() {
+        gameState = new GameState();
+        playerController = new PlayerController(gameState);
+        players = gameState.getPlayers();
+        countries = gameState.getCountries();
+        continents = gameState.getContinents();
+        gameEngine = new GameEngine(gameState, null);
+
+
+
+        Player p1 = new Player("mahdieh", gameState, RandomStrategyBehavior.class);
+        p1.setReinforcement(3);
+        Player p2 = new Player("parsa", gameState, RandomStrategyBehavior.class);
+        p2.setReinforcement(3);
+
+        players.put(p1.getName(), p1);
+        players.put(p2.getName(), p2);
+
+
+        // add continents and countries
+        continents.put(1, new Continent(1, 3));
+
+        countries.put(2, new Country(2, "CANADA", 1));
+        countries.put(5, new Country(5, "SWEDEN", 1));
+        p1.addCountry(countries.get(2));
+        p1.addCountry(countries.get(5));
+
+        countries.put(3, new Country(3, "USA", 1));
+        countries.put(4, new Country(4, "France", 1));
+        countries.put(6, new Country(6, "DENMARK", 1));
+        p2.addCountry(countries.get(3));
+        p2.addCountry(countries.get(4));
+        p2.addCountry(countries.get(6));
+
+        // add connections
+        countries.get(2).addAdjacentCountry(3);
+        countries.get(2).addAdjacentCountry(4);
+        countries.get(2).addAdjacentCountry(6);
+        countries.get(5).addAdjacentCountry(3);
+        countries.get(5).addAdjacentCountry(4);
+        countries.get(5).addAdjacentCountry(6);
+        countries.get(4).addAdjacentCountry(3);
+        countries.get(4).addAdjacentCountry(6);
+
+
+        countries.get(2).setNumberOfReinforcements(4);
+        countries.get(5).setNumberOfReinforcements(3);
+
+        countries.get(3).setNumberOfReinforcements(1);
+        countries.get(4).setNumberOfReinforcements(7);
+        countries.get(6).setNumberOfReinforcements(5);
+
+        orderController = new OrderController(gameState, null);
+
+    }
+
+    @Test
+    @DisplayName("A player should deploy in Random country")
+    public void shouldDeployInRandomCountry() {
+        gameEngine.setGamePhase(new IssueDeployOrder(gameEngine));
+        Player player = players.get("mahdieh");
+        player.issueOrder();
+        assertEquals(1, player.getOrders().size());
+        Order order = player.nextOrder();
+        assertDoesNotThrow(() -> (DeployOrder) order);
+        DeployOrder deployOrder = (DeployOrder) order;
+        assertEquals(3, deployOrder.getNumReinforcements());
+        assertTrue(player.getCountryIds().contains(deployOrder.getCountryId()));
+    }
+
+    @Test
+    @DisplayName("player should attack randomly it's neighbor countries")
+    public void shouldRandomAttack() {
+        gameEngine.setGamePhase(new IssueOrdersPhase(gameEngine));
+        Player player = players.get("mahdieh");
+        player.issueOrder();
+        assertEquals(1, player.getOrders().size());
+        Order order = player.nextOrder();
+        assertDoesNotThrow(() -> (AdvanceOrder) order);
+        AdvanceOrder advanceOrder = (AdvanceOrder) order;
+        assertEquals(3, advanceOrder.getNumReinforcements());
+    }
+
+    @Test
+    @DisplayName("player should randomly getting one of it's countries")
+    public void gettingRandomOwnsCountry() {
+        gameEngine.setGamePhase(new IssueOrdersPhase(gameEngine));
+        Player player = players.get("mahdieh");
+        player.issueOrder();
+        int randomPlayerCountryId = player.getCountryIds().get(rand.nextInt(player.getCountryIds().size()));
+        assertTrue(player.getCountryIds().contains(randomPlayerCountryId));
+    }
+
+    @Test
+    @DisplayName("player should randomly getting one of it's neighbor's countries")
+    public void shouldChooseOneOfItsNeighborsCountries() {
+        gameEngine.setGamePhase(new IssueOrdersPhase(gameEngine));
+        Player player = players.get("mahdieh");
+        player.issueOrder();
+        int randomPlayerCountryId = player.getCountryIds().get(rand.nextInt(player.getCountryIds().size()));
+        int randomAdjacentCountry = player.getCountries().get(randomPlayerCountryId).getAdjacentCountries().get(rand.nextInt(player.getCountries().get(randomPlayerCountryId).getAdjacentCountries().size()));
+        assertTrue(player.getCountries().get(randomPlayerCountryId).getAdjacentCountries().contains(randomAdjacentCountry));
+    }
+
+
+
+}
