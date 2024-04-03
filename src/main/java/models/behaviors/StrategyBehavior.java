01213c9ff903e821ab65dac90b7532470803fa45
assassin008 <djdkdc1234@gmail.com>
2024-04-02 22:06:30 -0400

    add String to Behavior class map in StrategyBehavior.

--- src/main/java/models/behaviors/StrategyBehavior.java
+++ src/main/java/models/behaviors/StrategyBehavior.java
@@ -13,6 +13,14 @@ public abstract class StrategyBehavior {
     Player d_player;
     GameState d_gameState;
 
+    public static Map<String, Class<? extends StrategyBehavior>> STR_TO_BEHAVIOR = Map.of(
+            "human", HumanStrategyBehavior.class,
+            "aggressive", AggressiveStrategyBehavior.class,
+            "benevolent", BenevolentStrategyBehavior.class
+//            "random", RandomStrategyBehavior.class,
+//            "cheater", CheaterStrategyBehavior.class
+    );
+
     public StrategyBehavior(Player p_player, GameState p_gameState) {
         d_player = p_player;
         d_gameState = p_gameState;
