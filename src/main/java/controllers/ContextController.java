07a5f50b811613993d71f3c720047da2dbc55753
FaridFarahmand <faridfarahmandf2f@gmail.com>
2024-04-09 12:58:16 -0400

    java doc context controller

--- src/main/java/controllers/ContextController.java
+++ src/main/java/controllers/ContextController.java
@@ -4,10 +4,13 @@ import models.GameContext;
 import models.GameState;
 import phases.GameEngine;
 
+/**
+ * The controller class for managing game context, including loading and saving games.
+ */
 public class ContextController {
     /**
      * handles the loadgame command
-     * @param p_args
+     * @param p_args p_args an array containing the file path of the saved game
      * @return true if game loaded successfully
      */
     public static boolean handleLoadGame(String[] p_args) {
@@ -22,6 +25,14 @@ public class ContextController {
         }
         return false;
     }
+
+    /**
+     * Handles the savegame command.
+     *
+     * @param p_args an array containing the file path to save the game
+     * @param p_gameState the current game state to be saved
+     * @param p_gameEngine the game engine controlling the game
+     */
 
     public static void handleSaveGame(String[] p_args, GameState p_gameState, GameEngine p_gameEngine) {
         System.out.println("Saving game...");
